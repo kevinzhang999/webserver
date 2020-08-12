dcb153e7f23208461371536f2caf19d6a3b52c1b
kevinzhang999 <51693342+kevinzhang999@users.noreply.github.com>
2020-08-13 10:59:15 +1200

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -4,7 +4,7 @@ apt-get -y update
 
 # install Apache2
 apt-get -y install apache2
-#apt-get -y install mysql-server
+# apt-get -y install mysql-server
 # apt-get -y install php libapache2-mod-php php-mysql
 
 echo \<center\>\<h1\>Hello Plexure!\</h1\>\<br/\>\</center\> > /var/www/html/plexure.html
